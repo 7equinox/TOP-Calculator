3635d9141eadde58838f3e548d0ef837ba5b64ae
Julius Evan C. Estonilo <116896327+7equinox@users.noreply.github.com>
2025-09-02 21:22:11 +0800

    Add and test operate function

--- script.js
+++ script.js
@@ -18,3 +18,26 @@ let num1;
 let operator;
 let num2;
 
+function operate(num1, operator, num2) {
+    switch(operator) {
+        case '+': return add(num1, num2);
+        case '-': return subtract(num1, num2);
+        case '*': return multiply(num1, num2);
+        case '/': return divide(num1, num2);
+    }
+}
+
+num1 = 10;
+operator = '*'
+num2 = 5;
+
+console.log( operate(num1, '+', num2) ); // 15
+console.log( operate(num1, '-', num2) ); // 5
+console.log( operate(num1, operator, num2) ); // 50
+console.log( operate(num1, '/', num2) ); // 2
+
+/*
+CONTINUE...
+3. Create a new function operate that takes an operator and two numbers
+and then calls one of the above functions on the numbers.
+*/
