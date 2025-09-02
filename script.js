476797c048983bb81bf4b78a39c36976d19c7595
Julius Evan C. Estonilo <116896327+7equinox@users.noreply.github.com>
2025-09-02 18:04:46 +0800

    Create MDAS functions

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("hello calculator");
+function add(addend1, addend2) {
+    return addend1 + addend2;
+}
+
+function subtract(minuend, subtrahend) {
+    return minuend - subtrahend;
+}
+
+function multiply(multiplicand, multiplier) {
+    return multiplicand * multiplier;
+}
+
+function divide(dividend, divisor) {
+    return dividend / divisor;
+}
+
