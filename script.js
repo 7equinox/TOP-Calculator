9a339f765578a7226ca274744697ce886788021a
Julius Evan C. Estonilo <116896327+7equinox@users.noreply.github.com>
2025-09-02 22:26:48 +0800

    make the calc work with 2 values and one op

--- script.js
+++ script.js
@@ -29,20 +29,54 @@ function operate(num1, operator, num2) {
 
 const inputDisplay = document.querySelector('#input-display');
 // console.log(inputDisplay);
-const listBtnDigits = document.querySelectorAll('.btn-digit');
-// console.log(listBtnDigits);
-const ARR_NUM_DIGITS = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0];
+const listBtnCalc = document.querySelectorAll('button');
+// console.log(listBtnCalc);
+const ARR_NUM_OP_VAL = [7, 8, 9, '*',
+                        4, 5, 6, '/',
+                        1, 2, 3, '+',
+                        'clear', 0, '=', '-'];
 
-let strNumDisplay = '';
+let strDsplyCalc = '';
 
-listBtnDigits.forEach((btnDigit, numIdx) => {
+listBtnCalc.forEach((btnDigit, numIdx) => {
     // console.log(btnDigit.id);
 
     btnDigit.addEventListener('click', () => {
         // console.log(btnDigit.id[4]);
-        inputDisplay.value += ARR_NUM_DIGITS[numIdx];
-        strNumDisplay = inputDisplay.value;
-        console.log(strNumDisplay);
+        switch(ARR_NUM_OP_VAL[numIdx]) {
+            case '*':
+            case '/':
+            case '+':
+            case '-':
+                operator = ARR_NUM_OP_VAL[numIdx];
+                break;
+            case '=':
+                // for (let i = 0; i < strDsplyCalc.length; i++) {
+                //     console.log(typeof i);
+                // }
+                [ num1, num2 ] = strDsplyCalc.split(operator);
+                // console.log("NUM1: " + num1);
+                // console.log("OP: " + operator);
+                // console.log("NUM2: " + num2);
+                numResult = operate(Number(num1), operator, Number(num2));
+                // console.log( numResult );
+                inputDisplay.value = numResult;
+                strDsplyCalc = '';
+                operator = '';
+                // console.log(strDsplyCalc);
+                return;
+            case 'clear':
+                inputDisplay.value = '';
+                strDsplyCalc = '';
+                operator = '';
+                // console.log(strDsplyCalc);
+                return;
+        }
+        
+        inputDisplay.value += ARR_NUM_OP_VAL[numIdx];
+        strDsplyCalc = inputDisplay.value;
+        // console.log(strDsplyCalc);
+        // console.log("OP: " + operator);
     });
 });
 
