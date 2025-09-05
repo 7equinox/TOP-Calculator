5aeab4776e58b2b363fdf9fedc1f2a24a658ffda
Julius Evan C. Estonilo <116896327+7equinox@users.noreply.github.com>
2025-09-05 13:23:59 +0800

    Initialize adding keyboard support

--- script.js
+++ script.js
@@ -106,120 +106,214 @@ function toggleBtnDecimal() {
     }
 }
 
-listBtnCalc.forEach((btnCalc, numIdx) => {
-    btnCalc.addEventListener('click', () => {
-        const calcBtnVal = ARR_CALC_BTN_VAL[numIdx];
+// Function to handle the main logic of the system
+function handleCalcButtons({
+    btnCalc = null,
+    numIdx = NaN,
+    calcBtnVal = null
+}) {
+    // If key pressed is not given, handle click events
+    if (calcBtnVal === null) {
+        calcBtnVal = ARR_CALC_BTN_VAL[numIdx];
+    }
 
-        switch (calcBtnVal) {
-            case 'clear':
-                removeBtnClckEffect();
+    switch (calcBtnVal) {
+        case 'clear':
+            removeBtnClckEffect();
+            clearDisplayAndData();
+            return;
+        
+        case 'backspace':
+            // Start new calc when a result is displayed & press backspace
+            if (strLastClckVal === '=') {
                 clearDisplayAndData();
-                return;
-            
-            case 'backspace':
-                // Start new calc when a result is displayed & press backspace
-                if (strLastClckVal === '=') {
-                    clearDisplayAndData();
-                }
+            }
 
-                // Keep default display value '0' if empty
-                if (inputDisplay.value !== '0') {
-                    inputDisplay.value = inputDisplay.value.slice(0, -1);
-                    if (inputDisplay.value === '') {
-                        inputDisplay.value = '0';
-                    }
-                    toggleBtnDecimal();
-                }
-                break;
-
-            case '=':
-                removeBtnClckEffect();
-
-                if (num1 === Infinity || // Undefined output
-                    strLastClckVal === null || // Invalid (only clicks '=')
-                    ARR_OPERATORS.includes(strLastClckVal)) {
-                    return;
-
-                } else if (Number.isNaN(num1) || // Only input 1st num then '='
-                           strLastClckVal === '=') { // Calc output & 2nd num
-                    num1 = parseFloat(inputDisplay.value);
-
-                } else { // Operate 1st input number with 2nd input number
-                    num2 = parseFloat(inputDisplay.value);
-                }
-
-                if (operator !== null && // Include operation sign
-                    !Number.isNaN(num2)) { // Defined 2nd input number
-                    num1 = operate(num1, operator, num2);
-                }
-                handleInvalidOutput();
-                toggleBtnDecimal();
-                break;
-
-            case '*':
-            case '/':
-            case '+':
-            case '-':
-                removeBtnClckEffect();
-                
-                // Undefined output
-                if (num1 === Infinity) {
-                    return;
-                }
-
-                btnCalc.classList.add('btn-clck');
-
-                // Evaluate more than a single pair of numbers at a time
-                if (operator !== null &&
-                    ARR_DIGITS.includes(strLastClckVal) &&
-                    !Number.isNaN(num1)) {
-                    num2 = parseFloat(inputDisplay.value);
-                    num1 = operate(num1, operator, num2);
-                    handleInvalidOutput();
-                    toggleBtnDecimal();
-                }
-                num1 = parseFloat(inputDisplay.value);
-                operator = calcBtnVal;
-                btnDecimal.disabled = false;
-                break;
-
-            default:
-                // Undefined output
-                if (num1 === Infinity) {
-                    return;
-                }
-
-                // Start new calc when a result is displayed & press new digit
-                if (strLastClckVal === '=') {
-                    clearDisplayAndData();
-                }
-
-                // Display another number given an operator is picked
-                if (ARR_DIGITS.includes(calcBtnVal) &&
-                    ((operator !== null &&
-                    ARR_OPERATORS.includes(strLastClckVal)) ||
-                    // Prevent leading zeroes
-                    inputDisplay.value === '0')) {
-                    inputDisplay.value = '';
-                } else if (calcBtnVal === '.' && (
-                    operator !== null &&
-                    ARR_OPERATORS.includes(strLastClckVal))) {
+            // Keep default display value '0' if empty
+            if (inputDisplay.value !== '0') {
+                inputDisplay.value = inputDisplay.value.slice(0, -1);
+                if (inputDisplay.value === '') {
                     inputDisplay.value = '0';
                 }
-                
-                inputDisplay.value += calcBtnVal;
-                if (calcBtnVal === '.') {
-                    btnDecimal.disabled = true;
-                }
-        }
-        strLastClckVal = calcBtnVal;
+                toggleBtnDecimal();
+            }
+            break;
 
-        console.log("=========================");
-        console.log("inputDisplay.value = " + inputDisplay.value);
-        console.log("num1 = " + num1);
-        console.log("operator = " + operator);
-        console.log("num2 = " + num2);
-        console.log("strLastClckVal = " + strLastClckVal);
+        case '=':
+            removeBtnClckEffect();
+
+            if (num1 === Infinity || // Undefined output
+                strLastClckVal === null || // Invalid (only clicks '=')
+                ARR_OPERATORS.includes(strLastClckVal)) {
+                return;
+
+            } else if (Number.isNaN(num1) || // Only input 1st num then '='
+                        strLastClckVal === '=') { // Calc output & 2nd num
+                num1 = parseFloat(inputDisplay.value);
+
+            } else { // Operate 1st input number with 2nd input number
+                num2 = parseFloat(inputDisplay.value);
+            }
+
+            if (operator !== null && // Include operation sign
+                !Number.isNaN(num2)) { // Defined 2nd input number
+                num1 = operate(num1, operator, num2);
+            }
+            handleInvalidOutput();
+            toggleBtnDecimal();
+            break;
+
+        case '*':
+        case '/':
+        case '+':
+        case '-':
+            removeBtnClckEffect();
+            
+            // Undefined output
+            if (num1 === Infinity) {
+                return;
+            }
+
+            // If key pressed, use button operator nodelist element
+            if (btnCalc === null) {
+                listBtnOprtr.forEach(btnOperator => {
+                    if ((btnOperator.textContent === '×' &&
+                        calcBtnVal === '*') ||
+                        btnOperator.textContent === calcBtnVal) {
+                        btnOperator.classList.add('btn-clck');
+                    }
+                });
+            } else {
+                btnCalc.classList.add('btn-clck');
+            }
+            
+            // Evaluate more than a single pair of numbers at a time
+            if (operator !== null &&
+                ARR_DIGITS.includes(strLastClckVal) &&
+                !Number.isNaN(num1)) {
+                num2 = parseFloat(inputDisplay.value);
+                num1 = operate(num1, operator, num2);
+                handleInvalidOutput();
+                toggleBtnDecimal();
+            }
+            num1 = parseFloat(inputDisplay.value);
+            operator = calcBtnVal;
+            btnDecimal.disabled = false;
+            break;
+
+        default:
+            // Undefined output
+            if (num1 === Infinity) {
+                return;
+            }
+
+            // Start new calc when a result is displayed & press new digit
+            if (strLastClckVal === '=') {
+                clearDisplayAndData();
+            }
+
+            // Display another number given an operator is picked
+            if (ARR_DIGITS.includes(calcBtnVal) &&
+                ((operator !== null &&
+                ARR_OPERATORS.includes(strLastClckVal)) ||
+                // Prevent leading zeroes
+                inputDisplay.value === '0')) {
+                inputDisplay.value = '';
+            } else if (calcBtnVal === '.' && (
+                operator !== null &&
+                ARR_OPERATORS.includes(strLastClckVal))) {
+                inputDisplay.value = '0';
+            }
+
+            // Only allowed one decimal point
+            if (calcBtnVal === '.' && !btnDecimal.disabled) {
+                btnDecimal.disabled = true;
+                inputDisplay.value += calcBtnVal;
+            }
+            if (calcBtnVal === '.' && btnDecimal.disabled) {
+                break;
+            }
+            inputDisplay.value += calcBtnVal;
+    }
+    strLastClckVal = calcBtnVal;
+
+    console.log("=========================");
+    console.log("inputDisplay.value = " + inputDisplay.value);
+    console.log("num1 = " + num1);
+    console.log("operator = " + operator);
+    console.log("num2 = " + num2);
+    console.log("strLastClckVal = " + strLastClckVal);
+}
+
+// Event listener for clicking buttons
+listBtnCalc.forEach((btnCalc, numIdx) => {
+    btnCalc.addEventListener('click', () => {
+        handleCalcButtons({ btnCalc, numIdx });
+        btnCalc.blur();
+    });
+});
+
+// Event listener for keyboard support
+document.addEventListener('keyup', (event) => {
+    const eventKeyup = event.key.toLowerCase();
+
+    switch (eventKeyup) {
+        case 'delete':
+            handleCalcButtons({ calcBtnVal: 'clear' });
+            break;
+
+        case 'backspace':
+            handleCalcButtons({ calcBtnVal: 'backspace' });
+            break;
+
+        case '=':
+        case 'enter':
+            handleCalcButtons({ calcBtnVal: '=' });
+            break;
+
+        case '*':
+        case '/':
+        case '+':
+        case '-':
+        case '.':
+            handleCalcButtons({ calcBtnVal: eventKeyup });
+            break;
+
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+            handleCalcButtons({ calcBtnVal: parseInt(eventKeyup) });
+    }
+
+    // For clicking effect buttons
+    listBtnCalc.forEach((btnCalc) => {
+        const boolHasPressDel = btnCalc.textContent === 'Clear' &&
+                                eventKeyup === 'delete';
+        const boolHasPressBckspc = btnCalc.textContent === 'Backspace' &&
+                                eventKeyup === 'backspace';
+        const boolHasPressEnter = btnCalc.textContent === '=' &&
+                                eventKeyup === 'enter';
+        const boolHasPressAstrsk = btnCalc.textContent === '×' &&
+                                eventKeyup === '*';
+        if (boolHasPressDel ||
+            boolHasPressBckspc ||
+            boolHasPressEnter ||
+            boolHasPressAstrsk ||
+            btnCalc.textContent === eventKeyup) {
+                btnCalc.classList.add('button-hover');
+                btnCalc.classList.add('button-active');
+            setTimeout(() => {
+                btnCalc.classList.remove('button-hover');
+                btnCalc.classList.remove('button-active');
+            }, 300);
+        }
     });
 });
 
@@ -249,6 +343,5 @@ clearDisplayAndData();
 // Guide: https://www.calculatorsoup.com/calculators/math/basic.php
 
 // Remaining:
-// 1) Refactor the code again (with testing)
-// 2) Add keyboard support
-// 3) Refactor the code again (with testing)
+// 1) Add keyboard support [INCLUDE THEM IN INPUT TEXT TYPE TO LISTEN]
+// 2) Refactor the code again (with testing)
